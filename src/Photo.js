8d785c04d13fa6b57ab168007542aaf71aeb57e0
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2019-05-23 12:59:06 -0300

    Ajustes de design

--- src/Photo.js
+++ src/Photo.js
@@ -7,7 +7,7 @@ function Photo() {
 
 	return (
 		<div className="row">
-			<div className="col-6 col-lg-2 offset-lg-5 offset-3 mt-5">
+			<div className="col-6 col-lg-2 offset-lg-5 offset-3 col-md-4 offset-md-4 mt-5">
 				<img src={imgPath} className="img-fluid me" alt="Rodrigo Zanotta" />
 			</div>
 		</div>
--- src/Social.js
+++ src/Social.js
@@ -8,23 +8,23 @@ function Social() {
 		<div className="row mt-5">
 
 			<div className="text-center mx-auto">
-				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="ml-3 mr-4" target="_blank">
+				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="ml-3 mr-3" target="_blank" title="GitHub">
 					<i className="fab fa-github fa-3x"></i>
 				</a>
 			
-				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="mr-4" target="_blank">
+				<a href="https://facebook.com/rodrigozanotta" rel="noopener noreferrer" className="mr-3" target="_blank" title="Facebook">
 					<i className="fab fa-facebook fa-3x"></i>
 				</a>
 			
-				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="mr-4" target="_blank">
+				<a href="https://twitter.com/zanottaa" rel="noopener noreferrer" className="mr-3" target="_blank" title="Twitter">
 					<i className="fab fa-twitter fa-3x"></i>
 				</a>
 			
-				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="mr-4" target="_blank">
+				<a href="https://instagram.com/zanottaa" rel="noopener noreferrer" className="mr-3" target="_blank" title="Instagram">
 					<i className="fab fa-instagram fa-3x"></i>
 				</a>
 			
-				<a href="https://github.com/zanotta" rel="noopener noreferrer" className="mr-4" target="_blank">
+				<a href="https://www.linkedin.com/in/rodrigozanotta" rel="noopener noreferrer" className="mr-3" target="_blank" title="Linkedin">
 					<i className="fab fa-linkedin fa-3x"></i>
 				</a>
 			</div>
