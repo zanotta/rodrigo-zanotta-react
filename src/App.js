f7fb5af4a461dd3e95a9e784d76a89e00f4c41d2
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2019-06-06 15:38:00 -0300

    E-mail e telefone.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import Photo from './Photo'
-import Presentation from './Presentation'
-import Social from './Social'
+import Photo from './Photo';
+import Presentation from './Presentation';
+import Social from './Social';
+import Contact from './Contact';
+import detectBrowserLanguage from 'detect-browser-language';
 import './App.css';
 
 class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			userLanguage: detectBrowserLanguage()
+		}
+
+	}
 
 	render(){
 
 		return (
 			<div className="container-fluid">
 				<Photo />
-				<Presentation />
+				<Presentation userLanguage={this.state.userLanguage} param2='Rodrigo' />
 				<Social />
+				<Contact />
 			</div>
 		);
 
---
+++ src/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+class Contact extends React.Component {
+
+	render(){
+
+		return (
+			<div className="row mt-5 mx-auto">
+
+				<div className="text-center col-12 col-lg-12">
+
+					<a href="mailto:contato@rodrigozanotta.com.br" rel="noopener noreferrer" target="_blank" title="E-mail">
+						contato@rodrigozanotta.com.br
+					</a>
+
+					<br />
+
+					55(51) 99877-0440
+
+				</div>
+
+			</div>
+		);
+
+	}
+
+}
+
+export default Contact;
