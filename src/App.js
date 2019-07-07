9373a3a24bed7f078dacffdacae5c33a07d5d12a
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2019-07-07 20:56:10 -0300

    React router adicionado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Social from './Social';
 import Contact from './Contact';
 import detectBrowserLanguage from 'detect-browser-language';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
 
@@ -17,15 +18,47 @@ class App extends React.Component {
 
 	}
 
-	render(){
-
+	Index(){
 		return (
 			<div className="container-fluid">
 				<Photo />
-				<Presentation userLanguage={this.state.userLanguage} param2='Rodrigo' />
+				<Presentation userLanguage={'pt-br'} param2='Rodrigo' />
 				<Social />
 				<Contact />
 			</div>
+		);
+	}
+
+
+	Sobre(){
+		return (
+			<div className="container-fluid">
+				<h2>Sobre Teste</h2>
+			</div>
+		);
+	}
+
+
+	render(){
+
+		return (
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/sobre/">Sobre</Link>
+							</li>
+						</ul>
+					</nav>
+
+					<Route path="/" exact component={this.Index} />
+					<Route path="/sobre/" component={this.Sobre} />
+				</div>
+			</Router>
 		);
 
 	}
